df1eb900d63c1f32da6e9ffb790e5a39d75da6e0
Shayna Stewart <shaynakstewart@gmail.com>
2018-03-30 13:09:42 -0700

    Add files via upload

---
+++ WelcomeInMessageDialogueBox.java
@@ -0,0 +1,22 @@
+/*
+ Name: Stewart, Shayna
+ Date: 2/11/16
+Project 1
+ */
+
+/**
+ *
+ * @author Student-HSLH133
+ */
+import javax.swing.JOptionPane;
+public class WelcomeInMessageDialogueBox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       JOptionPane.showMessageDialog(null, "Welcome to Java");
+    }
+    
+}
